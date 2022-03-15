38b8703284b5aa076a869c95cbdd112f9f0bea2f
Cley-gabriel <gabrielcley23@hotmail.com>
2022-03-15 16:58:50 -0300

    Código completo

---
+++ envia.py
@@ -0,0 +1,43 @@
+from string import Template
+from datetime import datetime
+from login import meu_email, minha_senha
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import smtplib
+
+
+
+with open('enviandoemail/template.html', 'r', encoding='utf-8') as html:
+    template = Template(html.read())
+    data_atual = datetime.now().strftime('%d/%m/%Y')
+    corpo_msg = template.substitute(nome='Cley', data=data_atual)
+
+#CONFIGURAÇÃO DO ENVIO DA MENSAGEM
+msg = MIMEMultipart()
+msg['from'] = 'NOME DE QUEM ENVIOU'
+msg['to'] = 'EMAIL DE QUEM VAI RECEBER SUA MENSAGEM'  
+msg['subject'] = 'ASSUNTO DO EMAIL'
+
+#MENSAGEM QUE DESEJA ENVIAR
+corpo = MIMEText('template.html', html)
+msg.attach(corpo)
+
+
+# ENVIO DE IMAGEM EM ANEXO
+# with open('IMAGEM.JPG', 'rb') as img:
+#     img = MIMEImage(img.read())
+#     msg.attach(img)
+
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    try:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(meu_email, minha_senha)
+        smtp.send_message(msg)
+        print('O e-mail foi enviado com sucesso.')
+    except Exception as erro:
+        print('O e-mail foi não enviado...')
+        print('Erro:', erro)
---
+++ login.py
@@ -0,0 +1,5 @@
+from getpass import getpass
+
+meu_email = input('E-mail: ')
+#Oculta a senha
+minha_senha = getpass(prompt='Senha: ', stream=None)
